668d41deffeed05eb19f629a440864ca3f04d631
Madan <upretymadan45@gmail.com>
2017-07-03 00:39:43 +1200

    enabled cors support using cors middleware

--- server.js
+++ server.js
@@ -2,16 +2,29 @@ var express = require("express");
 var open = require("open");
 var path = require("path");
 var chalk = require("chalk");
+var cors = require("cors");
 
 var port = 8080;
 
 var app = express();
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname,'/app/assets/')));
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,"/app/index.html"));
     console.log(process.cwd());
+});
+
+app.get('/products',function(req,res){
+    res.json(
+        [
+            {"id": 1,  "name":"cheese", "price":20, "imageUrl" :"../../assets/images/Cheese.png"},
+            {"id": 2, "name": "butter", "price":30, "imageUrl" :"../../assets/images/butter.png"},
+            {"id": 3, "name": "chicken Whole", "price":15, "imageUrl":"../../assets/images/Chicken.png"}
+        ]
+    );
 });
 
 
